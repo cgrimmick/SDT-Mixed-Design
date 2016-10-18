dabe02ab32f7cd2e59c29f6c24ba39caa619ef84
Christopher W Grimmick <chrisgrimmick@gmail.com>
2016-10-18 17:40:41 -0400

    Update ChangingProbggplot.R
    Smoothed probabilities added to plotting code, added fancy input options to loop through files

--- Analysis/ChangingProbggplot.R
+++ Analysis/ChangingProbggplot.R
@@ -1,20 +1,26 @@
-plotDat <- read.csv("~/Documents/Lab/Landy/SDT-Changing-Probabilities/Mixed-Design/Data/SDTChangingProbabilitiesMixed_HHL.csv",header = TRUE)
-subName <- "EGC"
 
 require("ggplot2")
 
+# !!! Because of loop, currently doesn't print the figures out for each subject
 
-# path = "~/Documents/Lab/Landy/SDT-Changing-Probabilities/Mixed-Design/Data/"
-# setwd(path)
-# file.names <- dir(pattern ="*.csv")
-# # 
-#  for(s in 1:length(file.names)){
-#   
-#   plotDat <- read.csv(file.names[s], header = TRUE)
-#    subName <- substr(file.names[s],31,33)
+path = "~/Documents/Lab/Landy/SDT-Changing-Probabilities/Mixed-Design/Data/"
+setwd(path)
+subj = toupper(readline(prompt = "Subject initials (ALL for all files): "))
+if(subj=="ALL"){
+  file.names <- dir(pattern ="*.csv")
+} else {
+  file.names  <- dir(pattern = paste(subj,"*.csv"))
+}
+
+save_opt = toupper(readline(prompt = "Save plots to file? "))
+
+for(s in 1:length(file.names)){
+   
+  plotDat <- read.csv(file.names[s], header = TRUE)
+  subName <- substr(file.names[s],31,33)
 #yrange <- c(min(plotDat[,c("estCrit","obsCrit")], na.rm = TRUE),max(plotDat[,c("estCrit","obsCrit")],na.rm = TRUE))
 
-#dev.new(width = 10, height = 6)
+dev.new(width = 10, height = 6)
 gp <- ggplot() +
   geom_line(data = plotDat, aes(x = trial, y = omCrit, linetype="Omniscient")) +
   geom_point(data = plotDat, aes(x=OvTrial,y=obsCrit, colour='Overt'), shape=1) +
@@ -26,9 +32,10 @@ gp <- ggplot() +
   labs(title=subName, x = "Trial", y = "Criterion (degrees)") 
 gp
 
-filename = paste(subName, "critbytrial.png", sep = "_")
-ggsave(filename=filename, plot=gp, width = 10, height = 6) 
-
+  if(save_opt=="Y"){
+    filename = paste(subj, "critbytrial.png", sep = "_")
+     ggsave(filename=filename, plot=gp, width = 10, height = 6) 
+  }
 # om.crit.rng <- c(min(plotDat$omCrit)-25,max(plotDat$omCrit)+25)
 yrange <- c(min(plotDat[,c("estCrit","obsCrit")], na.rm = TRUE),max(plotDat[,c("estCrit","obsCrit")],na.rm = TRUE))
 
@@ -37,7 +44,7 @@ yrange <- c(min(plotDat[,c("estCrit","obsCrit")], na.rm = TRUE),max(plotDat[,c("
 # om.crit.rng  <- c(-10,100) 
 # yrange <- c(-100,100)
 
-#dev.new(width = 6, height = 6)
+dev.new(width = 6, height = 6)
 gp2 <- ggplot() +
   geom_point(data = plotDat, aes(x=OvOmCrit,y=obsCrit, colour = "Overt"), shape=1) +
   geom_point(data = plotDat, aes(x=CovOmCrit,y=estCrit, colour = "Covert"), shape=1) +
@@ -52,28 +59,32 @@ gp2 <- ggplot() +
   labs(title=subName, x="Omniscient Criterion", y="Observed Criterion") 
 
 gp2
-filename = paste(subName, "obsVom.png", sep = "_")  
-ggsave(filename=filename, plot=gp2, width = 6, height = 6)
 
-# try plotting responses with acual probs (smoothed)
+  if(save_opt=="Y"){
+    filename = paste(subName, "obsVom.png", sep = "_")  
+    ggsave(filename=filename, plot=gp2, width = 6, height = 6)
+  }
 
-x <- plotDat$CovResponse - 1
-cx <- cumsum(x)
-n <- 24
-rsum <- (cx[n+1:length(x)] - cx[1:(length(x)-n)]) / n 
-prob.choose <- rsum
-pA  <- plotDat$Cov_pA
-wind <- plotDat$wind
-trial <- plotDat$CovTrial
-resp <- plotDat$CovResponse - 1
-#trial  <- 1:960
 
+dev.new(width = 11, height = 6)
 gp3 <- ggplot() +
-  geom_point(data = plotDat, aes(x = CovTrial, y=resp, colour= "Response"), size = 1.5) +
-  geom_point(data = plotDat, aes(x=wind, y=prob.choose, colour = "P(Choose A)"), size = 0.5) +
-  geom_smooth(data = plotDat, aes(x= CovTrial, y= pA, colour = "P(A)"), size = 0.5, se = FALSE, fullrange = TRUE) +
-  scale_x_continuous(expand = c(0,0), limits=c(1,1200)) + scale_y_continuous(expand = c(0,0), limits=c(0,1))
-
+  geom_point(data = plotDat, aes(x = CovTrial, y=catResp, shape = "Response"), colour = "blue", size = 2) +
+  geom_line(data = plotDat, aes(x=CovTrial, y=smoothCatResp, colour = "P(Choose A)")) +
+  geom_line(data = plotDat, aes(x= OvTrial, y= smoothpA_obs, colour = "P(A) Obs")) +
+  geom_line(data = plotDat, aes(x = trial, y = smoothProbA, linetype = "True P(A)")) + 
+  scale_x_continuous(expand = c(0,0), limits=c(1,1200)) + scale_y_continuous(expand = c(0,0), limits=c(0,1)) + 
+  theme_bw() +  theme(plot.background = element_blank(), plot.margin=unit(c(1,0,1,1),"cm")) +
+  scale_shape_manual("", breaks= "Response", values=1) + # would be good to change transparency
+  scale_colour_manual("", breaks=c("P(A) Obs","P(Choose A)"), values=c("red","blue")) +
+  scale_linetype_manual("", breaks=c("True P(A)","P(Choose A)","P(A) Obs"), values=c("dashed","solid","solid")) +
+  labs(title=subName, x="Trial", y="Probability") 
 
 gp3
 
+  if(save_opt=="Y"){
+    filename = paste(subName, "smoothed_probs.png", sep = "_")  
+    ggsave(filename=filename, plot=gp3, width = 11, height = 6)
+  }
+
+
+}
